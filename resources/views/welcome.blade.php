12ce662420ff65933bd19426e5563995cb78abb8
Ashley Marie Callahan <ashley.callahan1091@gmail.com>
2016-08-29 15:53:23 -0500

    added bootstrap, created broken master.blade - please hold while the appropriate code is written

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,4 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>Laravel</title>
+        @extends('master.blade')
 
         <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
 
@@ -41,5 +38,4 @@
                 <div class="title">Laravel 5</div>
             </div>
         </div>
-    </body>
-</html>
+

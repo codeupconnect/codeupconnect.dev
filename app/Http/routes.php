67d7313c0eefada764d6f631a2c0e737e66f4091
Eddie Knight <beijingtexan@gmail.com>
2016-08-30 15:08:59 -0500

    added functionality to AuthController; created route for /login and /auth

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -10,3 +10,11 @@
 // Route::get('/', 'HomeController@showWelcome');
 Route::get('/', function () {
     return view('welcome');
+
+Route::get('/login', function() {
+	return view('login');
+})
+
+Route::get('/auth', function() {
+	return view('auth')
+})

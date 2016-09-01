129867e0879ebfc7a3d4a506acd7a95fe71cd0da
Eddie Knight <iv.eddieknight@gmail.com>
2016-09-01 10:07:41 -0500

    troubleshooting UsersController

--- resources/views/showusers.blade.php
+++ resources/views/showusers.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 
-	 <div class="container">
+<!-- 	<div class="container">
 	 	<div class="row">
 	 		<div class="col-sm-4" data-value="{{ session('nickname') }}">
 	 			<h1 class="{{ session('nickname') }}"> {{session('nickname')}} </h1>
@@ -10,10 +10,10 @@
 	 		<div class="col-sm-4" data-value="{{ session('nickname') }}">
 	 			<h1 class="{{ session('nickname') }}"> {{session('nickname')}} </h1>
 	 		</div>
-	 	</div>
+	 	</div> -->
 @section('bottom-scripts')
 	<script>
-	 	var name = $(".{{ session('nickname') }}").data();
+	 	// var name = $(".{{ session('nickname') }}").data();
 	 	//really not sure what to do with this ^
 	 	//will eventually need to loop through all the database user names
 	 	//and use the nickname to find the api content & return the JSON

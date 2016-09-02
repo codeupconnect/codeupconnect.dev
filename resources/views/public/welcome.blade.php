d3f44f03b32bd76f65e73f1aae4582e49ab8dbeb
Ashley Marie Callahan <ashley.callahan1091@gmail.com>
2016-09-02 13:30:30 -0500

    still building homepage - will try to finish by 5pm

--- resources/views/public/welcome.blade.php
+++ resources/views/public/welcome.blade.php
@@ -5,7 +5,7 @@
         <div class="slide story" id="slide-1" data-slide="1">
 		<div class="container">
 			<div id="home-row-1" class="row clearfix">
-				<div class="col-xs-12">
+				<div class="col-12">
 					<h1 class="font-thin">Codeup<span class="font-semibold">Connect</span></h1>
 					<h4 class="font-thin">We are a team of <span class="font-semibold">Codeup graduates</span> looking to give back to the community.</h4>
 					<br>
@@ -13,7 +13,7 @@
 				</div><!-- /col-12 -->
 			</div><!-- /row -->
 			<div id="home-row-2" class="row clearfix">
-				<div class="col-12 col-sm-4"><img src="images/CWYK8CLC61_Fotor.jpg"></div>
+				<div class="col-12 col-xs-4"><img src="images/CWYK8CLC61_Fotor.jpg"></div>
 			</div><!-- /row -->
 		</div><!-- /container -->
 	</div><!-- /slide1 -->
@@ -22,37 +22,66 @@
 	<div class="slide story" id="slide-2" data-slide="2">
 		<div class="container">
 			<div class="row title-row">
-				<div class="col-12 font-thin">Contrary to popular belief, <span class="font-semibold">Lorem Ipsum</span> is not simply random text.</div>
+				<div class="col-12 font-semibold">Our Mission</div>
+			</div><!-- /row -->
+			<div class="row line-row">
+				<div class="hr">&nbsp;</div>
+			</div><!-- /row -->
+			
+			<div class="row content-row">
+				<div class="col-12">
+					<h3>Blah Blah Blah Space Filler Blah Blah Blah Space Filler Blah Blah Blah Space Filler Blah Blah Blah Space Filler Blah Blah Blah Space Filler Blah Blah Blah Space Filler Blah Blah Blah Space Filler Blah Blah Blah Space Filler Blah Blah Blah Space Filler Blah Blah Blah Space Filler Blah Blah Blah Space Filler Blah Blah Blah Space Filler Blah Blah Blah Space Filler Blah Blah Blah Space Filler Blah Blah Blah Space Filler Blah Blah Blah Space Filler Blah Blah Blah Space Filler Blah Blah Blah Space Filler Blah Blah Blah Space Filler Blah Blah Blah Space Filler Blah Blah Blah Space Filler Blah Blah Blah Space Filler Blah Blah Blah Space Filler Blah Blah Blah Space Filler Blah Blah Blah Space Filler Blah Blah Blah Space Filler Blah Blah Blah Space Filler Blah Blah Blah Space Filler Blah Blah Blah Space Filler Blah Blah Blah Space Filler Blah Blah Blah Space Filler Blah Blah Blah Space Filler Blah Blah Blah Space Filler Blah Blah Blah Space Filler Blah Blah Blah Space Filler Blah Blah Blah Space Filler </h3>
+				</div><!-- /col12 -->
+			</div><!-- /row -->
+		</div><!-- /container -->
+	</div><!-- /slide2 -->
+
+		<!-- === SLide 3 - Portfolio -->
+	<div class="slide story" id="slide-3" data-slide="3">
+		<div class="container">
+			<div class="row title-row">
+				<div class="col-xs-12 font-thin"><span class="font-semibold">Portfolio</div>
 			</div><!-- /row -->
 			<div class="row line-row">
 				<div class="hr">&nbsp;</div>
 			</div><!-- /row -->
 			<div class="row subtitle-row">
-				<div class="col-12 font-thin">This is what <span class="font-semibold">we do best</span></div>
+				<div class="col-xs-1 hidden-xs">&nbsp;</div>
+				<div class="col-xs-12 col-xs-10 font-light"><p>Here's where we brag about clients we've worked with...whenever that actually happens.</p> <p>For now...dancing Schmidty.</p></div>
+				<div class="col-xs-1 hidden-xs">&nbsp;</div>
 			</div><!-- /row -->
-			<div class="row content-row">
-				<div class="col-12 col-lg-3 col-sm-6">
-					<p><i class="icon icon-eye-open"></i></p>
-					<h2 class="font-thin">Visual <span class="font-semibold">Identity</span></h2>
-					<h4 class="font-thin">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</h4>
-				</div><!-- /col12 -->
-				<div class="col-12 col-lg-3 col-sm-6">
-					<p><i class="icon icon-laptop"></i></p>
-					<h2 class="font-thin">Web <span class="font-semibold">Design</span></h2>
-					<h4 class="font-thin">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</h4>
-				</div><!-- /col12 -->
-				<div class="col-12 col-lg-3 col-sm-6">
-					<p><i class="icon icon-tablet"></i></p>
-					<h2 class="font-thin">Mobile <span class="font-semibold">Apps</span></h2>
-					<h4 class="font-thin">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</h4>
-				</div><!-- /col12 -->
-				<div class="col-12 col-lg-3 col-sm-6">
-					<p><i class="icon icon-pencil"></i></p>
-					<h2 class="font-semibold">Development</h2>
-					<h4 class="font-thin">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</h4>
-				</div><!-- /col12 -->
+			<div class="row content-row col-xs-12">
+				
+				<div class="col-xs-6 img-responsive"><img src="giphy.gif" alt=""></div>
+				<div class="col-xs-6 img-responsive"><img src="giphy.gif" alt=""></div>
+				
 			</div><!-- /row -->
 		</div><!-- /container -->
-	</div><!-- /slide2 -->
+	</div><!-- /slide5 -->
+
+	<!-- === Slide 4 - Process === -->
+	<div class="slide story" id="slide-4" data-slide="4">
+		<div class="container">
+			<div class="row title-row">
+				<div class="col-xs-12 font-thin">Featured Freelancers</span></div>
+			</div><!-- /row -->
+			<div class="row line-row">
+				<div class="hr">&nbsp;</div>
+			</div><!-- /row -->
+			<div class="row subtitle-row">
+				<div class="col-xs-1 hidden-xs">&nbsp;</div>
+				<div class="col-xs-12 col-xs-10 font-light"><p>And when we have freelancers to display, this is where they'd go!</p>
+				<p>Until then...more Dancing Schmidty.</p>
+				</div>
+				
+		
+				<div class="col-xs-12 img-responsive"><img src="giphy.gif" alt=""></div>
+		
+				<div class="col-xs-1 hidden-xs">&nbsp;</div>
+			</div><!-- /row -->
+		</div><!-- /container -->
+	</div><!-- /slide4 -->
+
+
 
 @stop

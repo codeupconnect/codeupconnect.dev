5c6f5cde0cddc93f69d74e8a0d588550c0a4dae9
Eddie Knight <iv.eddieknight@gmail.com>
2016-09-02 09:30:40 -0500

    changed master file path to layout.master

--- resources/views/trello.blade.php
+++ resources/views/trello.blade.php
@@ -1,27 +1,94 @@
-@extends('master')
+@extends('layouts.master')
 
 @section('content')
     
-    <div class="container">
-        <h1>Trello Dashboard</h1>		        
-        <!-- We will be putting our dashboard right here -->		    
-    </div> 
+    <div class="container">		      
+    	<h1>Trello Dashboard</h1>		
+      	<form class="form-horizontal" id="boards_form">		        
+	      	<div class="form-group">		          
+	      		<label class="control-label">Choose your board</label>
+	      	    <select class="form-control" id="boards"></select>		        
+	      	</div>		      
+	    </form>
+	    <div id="labels"></div>
+    	  </div> 
+		    
+	</div> 
 
 @section('bottom-scripts')
-<script src="http://code.jquery.com/jquery-1.7.1.min.js"></script>
-<script src="https://api.trello.com/1/client.js?key=06255aa30ed43a51b57297877330a541"></script>
-<script>
-var authenticationSuccess = function() { console.log(Successful authentication); };
-var authenticationFailure = function() { console.log(Failed authentication); };
-Trello.authorize({
-  type: 'popup',
-  name: 'Getting Started Application',
-  scope: {
-    read: 'true',
-    write: 'true' },
-  expiration: 'never',
-  success: authenticationSuccess,
-  error: authenticationFailure
-});
-</script>
+
+	<script src="http://code.jquery.com/jquery-1.7.1.min.js"></script>
+	<script src="https://api.trello.com/1/client.js?key=06255aa30ed43a51b57297877330a541"></script>
+	<script>
+		
+		var authenticationSuccess = function() 
+			{ 
+				console.log('Successful authentication'); 
+			};
+		var authenticationFailure = function() 
+			{ 
+				console.log('Failed authentication'); 
+			};
+		
+	    var loadedBoards = function(boards) 
+	    {		      
+	    	$.each(boards, function(index, value) 
+	    	{		        
+	    		$('#boards')		          
+	    		.append($("<option></option>")		          
+	    		.attr("value",value.id)		          
+	    		.text(value.name)); 		      
+	    	});		    
+	    };		    
+
+		// Get the users boards		      
+		var loadBoards = function() 
+		{		      
+			Trello.get(
+				'/members/me/boards/',		        
+				loadedBoards,		        
+				function() { console.log("Failed to load boards"); }
+			);		    
+		};
+
+		var loadedLabels = function(labels) 
+		{
+			console.log(labels);
+      		$.each(labels, function(index, label) 
+      		{
+        		var label = $("<p><span class='badge' style='background:" + label.color + ";'>" + label.uses + "</span> " + label.id + "</p>");
+        		$('#labels').append(label);
+      		});
+    	};
+
+    	var dump = function(data)
+    	{
+    		console.log(data);
+    	}
+
+	    $('#boards').change(function() 
+	    {
+	    	var boardId = $("option:selected", this).val();
+	    	$('#labels').empty();
+
+	      
+	      	Trello.get(
+	        	'/boards/' + boardId,
+	        	dump,
+	       		function() { console.log("Failed to load labels"); }
+	      	);
+	    });
+
+		Trello.authorize({
+		  type: 'popup',
+		  name: 'CodeupConnect',
+		  scope: {
+		    read: 'true',
+		    write: 'true' },
+		  expiration: 'never',
+		  success: loadBoards,
+		  error: authenticationFailure
+		});
+
+	</script>
 @stop

fe7a894344c02b21b8c07e3ae917a5d2d4289f86
Scott Crawford <ascrawford@me.com>
2016-09-05 11:57:02 -0500

    Revision to Project Migration

--- database/migrations/2016_08_29_210144_create_projects_table.php
+++ database/migrations/2016_08_29_210144_create_projects_table.php
@@ -14,16 +14,29 @@ class CreateProjectsTable extends Migration
     {
         Schema::create('projects', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('client_name');
+            $table->string('status');
+            $table->string('organization_name');
             $table->string('site_url');
-            $table->longText('description');
             $table->date('start_date');
             $table->date('end_date');
-            $table->string('email');
-            $table->string('status');
-            $table->longText('preferred_tech');
-            $table->integer('phone');
             $table->string('point_person');
+            $table->integer('phone');
+            $table->string('email');
+            $table->longText('project_details');
+            $table->boolean('logo_graphics');
+            $table->boolean('color_palette');
+            $table->boolean('facebook');
+            $table->boolean('linkedin');
+            $table->boolean('twitter');
+            $table->boolean('youtube');
+            $table->boolean('instagram');
+            $table->boolean('tumblr');
+            $table->boolean('blog');
+            $table->boolean('comments_feedback');
+            $table->boolean('member_signup');
+            $table->boolean('contact_form');
+            $table->boolean('existing_database');
+            $table->boolean('payment_donations');
             $table->timestamps();
         });
     }

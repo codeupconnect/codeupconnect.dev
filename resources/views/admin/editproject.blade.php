80d4a86744b13ae736ff33bb498d092ca1696079
Ashley Marie Callahan <ashley.callahan1091@gmail.com>
2016-09-06 15:13:31 -0500

    created edit project blade

--- resources/views/admin/editproject.blade.php
+++ resources/views/admin/editproject.blade.php
@@ -1,1 +1,27 @@
-<!-- Approve/edit projects -->
+@extends('layout.master')
+
+@section('content')
+<h2 class="title">Edit Project</h2>
+
+<div class="container">	
+	<div class="form-container container">
+		<form>
+		  <div class="form-group">
+			<label>Project Name</label>
+			<input type="text" class="form-control" placeholder="Name">
+		  </div>
+		  <div class="radio">
+			<label>
+			  <input type="radio" name="project" value="accept"> Accept
+			</label>
+			<label>
+			  <input type="radio" name='project' value="reject"> Reject
+			</label>
+		  </div>
+		  <button type="submit" class="btn btn-default">Submit</button>
+		</form>
+	</div>
+</div>
+
+
+@stop

06b4c187fe859378dfd99c180edbd284ed1f0793
Eddie Knight <iv.eddieknight@gmail.com>
2016-09-06 17:07:08 -0500

    removed unneccary items

--- resources/views/admin/adminportal.blade.php
+++ resources/views/admin/adminportal.blade.php
@@ -11,22 +11,16 @@
 				<td>Status</td>
 				<td>Organization</td>
 				<td>Project Details</td>
-				<td>Edit/Accept</td>
-				<td>Reject</td>
 			</tr>
 		<thead>
 		<tbody>
 		@foreach($projects as $project)
 			<tr>
-				<td><a href="{{ action('ProjectsController@showProject', $project->id }}"></a></td>
+				<a href="{{ action('ProjectsController@showProject', $project->id }}"></a>
+				<td>{{ $project->project_id }}</td>
 				<td>{{ $project->status }}</td>
 				<td>{{ $project->organization_name }} </td>
 				<td>{{ $project->project_details }}</td>
-				<td><a href="{{ action('ProjectsController@edit', $project->id)}}">Edit/Accept</td>
-				<td><a href="{{ action('ProjectsController@destroy', $project->id)}}">
-					{{ csrf_field() }}
-	    			{{ method_field("DELETE")}}
-	    			<button class="btn btn-danger">Delete</button></td>
 			</tr>
 		@endforeach
 		</tbody>

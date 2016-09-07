e200e95381f56ee5c9f9067723770a89433c1c17
Ashley Marie Callahan <ashley.callahan1091@gmail.com>
2016-09-07 13:38:21 -0500

    changed footer because it was unnecessary

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -5,37 +5,20 @@
 	<hr>
 		<div class="row">
 			<div class="col-lg-12">
-				<div class="col-md-3">
-					<ul class="nav nav-pills nav-stacked">
-						<li><a href="#">About Us</a></li>
+				<div class="col-md-6">
+					<ul class="nav nav-stacked">
+						<li><a class="font-semibold" href="/">CodeupConnect</a></li>
 					</ul>
 				</div>
-				<div class="col-md-3">
-					<ul class="nav nav-pills nav-stacked">
-						<li><a href="#">Mission</a></li>
+				<div class="col-md-6">
+					<ul class="nav nav-stacked">
+						<li><a class="font-semibold" href="http://codeup.com/">Codeup</a></li>
 					</ul>
 				</div>
-				<div class="col-md-3">
-					<ul class="nav nav-pills nav-stacked">
-						<li><a href="#">Portfolio</a></li>	  
-					</ul>
-				</div>
-				<div class="col-md-3">
-					<ul class="nav nav-pills nav-stacked">
-						<li><a href="#">Featured Freelancers</a></li>
-					</ul>
-				</div>  
 			</div>
 		</div>
 	<hr>
-		<div class="row">
-			<div class="col-xs-12">
-				<ul class="nav nav-pills nav-justified">
-					<li><a href="/">CodeupConnect</a></li>
-					<li><a href="#">Codeup</a></li>
-				</ul>
-			</div>
-		</div>
+		
 	</div>
 </footer>
 

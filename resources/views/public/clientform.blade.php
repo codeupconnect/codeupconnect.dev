37b7a1871c16b5370a62953d1b853c5e37ad93f0
Scott Crawford <ascrawford@me.com>
2016-09-07 15:00:15 -0500

    Style for client form

--- resources/views/public/clientform.blade.php
+++ resources/views/public/clientform.blade.php
@@ -5,9 +5,12 @@
 <div class="container form-font">
     <h1 class="navbar-margin text-center">Submit a Project</h1>
 	<div class="container-fluid form-group">
-        <div class="row">
-            <form method="POST" action="{{ action('ProjectsController@store') }}">
+        <div class="col-sm-12">
+	    	<form class="form.horizontal" method="POST" action="{{ action('ProjectsController@store') }}">
+ 
                 {!! csrf_field() !!}
+                
+			    
                 <p>Your Organization: <input type="text" name="organization_name" value="{{ old('organization_name') }}" class="form-control"></p>
                 @if($errors->has('organization_name'))
 		    		{!! $errors->first('organization_name', '<p><span class="help-block alert alert-warning">:message</span></p>') !!}

f151989a742727f78081a07710922842cf3ed5f4
Ashley Marie Callahan <ashley.callahan1091@gmail.com>
2016-09-07 09:38:22 -0500

    formatted navbar

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -8,35 +8,33 @@
 				<span class="icon-bar"></span>
 				<span class="icon-bar"></span>
 			</button>
-			<a class="navbar-brand">CodeupConnect</a>
+			<a class="navbar-brand" href="/">CodeupConnect</a>
 		</div>
-
 		<!-- Collect the nav links, forms, and other content for toggling -->
-		
-
-			@if (isset($_SESSION))
-
-				<li class="dropdown">
-			 		<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Welcome<span class="caret"></span></a>
-					<ul class="dropdown-menu">
-						<li><a href="#">Profile</a></li>
-						<li><a href="#">Edit Profile</a></li>
-						<li><a href="#">Team Hub</a></li>
-						<li role="separator" class="divider"></li>
-						<li><a href="#">Logout</a></li>
-					</ul>
-				</li>	
-			@else
-				<div class="navbar-collapse collapse">
-				<ul class="nav navbar-nav">
-					<li data-slide="2"><a id="menu-link-2" href="#slide-2" title="Next Section">Mission </a></li>
-					<li><a>|</a></li>
-					<li data-slide="3"><a id="menu-link-3" href="#slide-3" title="Next Section">Portfolio</a></li>
-					<li><a>|</a></li>
-					<li data-slide="4"><a id="menu-link-4" href="#slide-4" title="Next Section">Featured Freelancers</a></li>
-					
-				</ul>
-				<ul class="nav navbar-nav navbar-right">
+	
+		@if (isset($_SESSION))
+		<li class="dropdown">
+	 		<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Welcome<span class="caret"></span></a>
+			<ul class="dropdown-menu">
+				<li><a href="/account">Profile</a></li>
+				<li><a href="/edit-account">Edit Profile</a></li>
+				<li><a href="#">Team Hub</a></li>
+				<li role="separator" class="divider"></li>
+				<li><a href="{{action('Auth\AuthController@logout')}}">Logout</a></li>
+			</ul>
+		</li>	
+		@else
+		<div class="navbar-collapse collapse">
+			<ul class="nav navbar-nav">
+				<li data-slide="2"><a id="menu-link-2" href="#slide-2" title="Next Section">Mission </a></li>
+				<li><a>|</a></li>
+				<li data-slide="3"><a id="menu-link-3" href="#slide-3" title="Next Section">Portfolio</a></li>
+				<li><a>|</a></li>
+				<li data-slide="4"><a id="menu-link-4" href="#slide-4" title="Next Section">Featured Freelancers</a></li>
+				<li><a>|</a></li>
+				<li><a href="#">Admin</a></li>
+			</ul>
+			<ul class="nav navbar-nav navbar-right">
 				<div>
 					<li>
 						<a href="/login" class="btn btn-block btn-social btn-lg btn-github">
@@ -45,7 +43,7 @@
 						</a>
 					</li>
 				</div>
-			@endif
+		@endif
 			</ul>
 		</div><!-- /.navbar-collapse -->
 	</div><!-- /.container-fluid -->

4b1f774281b2c6675a44598704cd887101d336e7
Ashley Marie Callahan <ashley.callahan1091@gmail.com>
2016-09-07 11:58:07 -0500

    made view for project invite

---
+++ resources/views/alumni/user-project-invite.blade.php
@@ -0,0 +1,20 @@
+@extends('layout.master')
+
+@section('content')
+
+<h2 class="title">Your Project, Should You Choose to Accept It</h2>
+
+<div class="container">	
+	
+	<h4>Project</h4>
+	<p><textarea>Yay project</textarea></p>
+</div>
+<div class="container button-container">	  
+	  <button type="button" class="btn btn-success" >Accept</button>
+	  <button type="submit" class="btn btn-danger" >Reject</button>
+</div>
+
+
+
+
+@stop

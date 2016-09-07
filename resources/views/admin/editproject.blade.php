535b1b0a03ddbf543876ea299ae693d59a0caba2
Eddie Knight <iv.eddieknight@gmail.com>
2016-09-06 21:26:17 -0500

    created minimum view for running update()

--- resources/views/admin/editproject.blade.php
+++ resources/views/admin/editproject.blade.php
@@ -1,32 +1,27 @@
 @extends('layout.master')
 
 @section('content')
-<h2 class="title">Edit Project</h2>
+<h2 class="title">Accept Project</h2>
 
 <div class="container">	
 	<div class="form-container container">
-		<h1 id="edit-data">Edit</h1>
-		<form>
-			@foreach ($project['attributes'] as $data)
-		  <div class="form-group">
-			<h2 class="static-option">{{ $data }}</h2>
-			<input name="{{ $data }}" style="display:none;" type="text" class="form-control edit-option" value="{{ $data }}" >
-		  </div>
-		  @endforeach
+		<h1 id="edit-data">Edit and Approve</h1>
+		<form method="POST" action="{{ action('ProjectsController@update', $boolean['id']) }}">
+            {!! csrf_field() !!}
+			@foreach ($data as $key => $value)
+				<div class="form-group">
+					<h3 class="static-option">{{ $key }}: {{ $value }}</h3>
+					<input name="{{ $key }}" style="display:none;" type="text" class="form-control edit-option" value="{{ $value }}" >
+				</div>
+			@endforeach
 
-		  @foreach ($boolean['attributes'] as $data)
-		  	<h4> {{ $data }} </h4>
-		  	@endforeach
-
-		  <div class="radio">
-			<label>
-			  <input type="radio" name="project" value="accept"> Accept
-			</label>
-			<label>
-			  <input type="radio" name='project' value="reject"> Reject
-			</label>
-		  </div>
-		  <button type="submit" class="btn btn-default">Submit</button>
+			@foreach ($boolean as $key => $value)
+				@if ($value == 0)
+			  	<h4> {{ $key }} </h4>
+			  	@endif
+			@endforeach
+		
+			<button type="submit" class="btn btn-default">Accept</button>
 		</form>
 	</div>
 </div>

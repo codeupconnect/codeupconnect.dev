4337f3a62a75b2a521c20d053cc23463370fde44
Ashley Marie Callahan <ashley.callahan1091@gmail.com>
2016-09-07 18:40:36 -0500

    ironed out design layout

--- resources/views/alumni/user.blade.php
+++ resources/views/alumni/user.blade.php
@@ -3,45 +3,56 @@
 @section('content')
 
 <div class="container">
-    <div class="row profile">
-        <div class="col-md-3">
-            <div class="profile-sidebar">
-                <!-- SIDEBAR USERPIC -->
-                <div class="profile-userpic">
-                    <img src="../bucky.jpg" class="img-responsive" alt="">
-                </div>
-                <!-- END SIDEBAR USERPIC -->
-                <!-- SIDEBAR USER TITLE -->
-                <div class="profile-usertitle">
-                    <div class="profile-usertitle-name">
-                        Bucky Barnes
-                    </div>
-                    <div class="profile-usertitle-job">
-                        Winter Soldier
-                    </div>
-                </div>
-                <!-- END SIDEBAR USER TITLE -->
-               
-                <!-- END SIDEBAR BUTTONS -->
-                <!-- SIDEBAR MENU -->
-                <div class="profile-usermenu">
-                    <ul class="nav">
-                        <li>
-                            <a href="#">
-                            <i class="glyphicon glyphicon-user"></i>
-                            Edit Profile </a>
-                        </li>
-                    </ul>
-                </div>
-                <!-- END MENU -->
-            </div>
-        </div>
-        <div class="col-md-9">
-            <div class="profile-content">
-               Some user related content goes here...
-            </div>
-        </div>
-    </div>
+	<div class="row profile">
+		<div class="col-md-3">
+			<div class="profile-sidebar">
+				<!-- Profile Pic -->
+				<div class="profile-userpic">
+					<img src="../bucky.jpg" class="img-responsive" alt="">
+				</div>
+				<!-- Username/Preference -->
+				<div class="profile-usertitle">
+					<div class="profile-usertitle-name">
+						Bucky Barnes
+					</div>
+					<div class="profile-usertitle-job">
+						Winter Soldier (prefers FE)
+					</div>
+				</div>
+
+				<!-- Menu -->
+				<div class="profile-usermenu">
+					<ul class="nav">
+						<li>
+							<a href="#">
+							<i class="fa fa-github"></i>
+							Github Username </a>
+						</li>
+						<li>
+							<a href="#">
+							<i class="glyphicon glyphicon-user"></i>
+							Edit Profile </a>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+
+		<div class="col-md-9">
+			<div class="profile-content">
+				<div class="container">
+					<h3>Current Project:</h3>
+					<div class="container">
+						<h4>Current Team:</h4>
+					</div>
+				</div>
+				<div class="container ">
+					<h3 class="cc-portfolio">CodeupConnect Portfolio</h3>
+				</div>
+
+			</div>
+		</div>
+	</div>
 </div>
 <br>
 <br>
@@ -53,13 +64,13 @@
 		var api = $('#user-info').data();
 		// api['value'] is now the string we are looking for
 
-	 	$.getJSON(api['value'], function (data) {
-            console.log(data);
+		$.getJSON(api['value'], function (data) {
+			console.log(data);
 
-            // Enter #id that should be targeted, and data['value'] that should be inserted
-	        $('#github-id').text(data['login']);
-	        $('#name').text(data['name']);
-        });
+			// Enter #id that should be targeted, and data['value'] that should be inserted
+			$('#github-id').text(data['login']);
+			$('#name').text(data['name']);
+		});
 
 
 	</script>

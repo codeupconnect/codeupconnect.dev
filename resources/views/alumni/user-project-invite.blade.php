a3cf59b657168b158a2600751c8dccee4e4596b4
Eddie Knight <iv.eddieknight@gmail.com>
2016-09-07 16:54:10 -0500

    tested  and , ready to use

--- resources/views/alumni/user-project-invite.blade.php
+++ resources/views/alumni/user-project-invite.blade.php
@@ -1,18 +1,15 @@
 @extends('layout.master')
 
 @section('content')
-
 <h2 class="title">Your Project, Should You Choose to Accept It</h2>
 
 <div class="container">	
 	
-	<h4>Project</h4>
-	<p><textarea>Yay project</textarea></p>
+	<h4>{{ $user->name }}</h4>
+	<p>{{ $project->organization_name }}</p>
+	<p>{{ $project->project_details }}</p>
 </div>
-<div class="container button-container">	  
-	  <button type="button" class="btn btn-success" href="{{ action('UsersController@acceptInvite', $user->id) }}">Accept</button>
-	  <button type="submit" class="btn btn-danger" href="{{ action('UsersController@rejectInvite', $user->id) }}">Reject</button>
-</div>
+
 
 
 

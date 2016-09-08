2da1a6cd06f24e1b973d82e6b84a7dc993c31b96
Eddie Knight <iv.eddieknight@gmail.com>
2016-09-08 12:26:01 -0500

    moved acceptInvite() to user model

--- app/User.php
+++ app/User.php
@@ -19,4 +19,36 @@ class User extends Model implements AuthenticatableContract,
     protected $fillable = ['name', 'email', 'nickname', 'url', 'github_id', 'avatar'];
 
     protected $hidden = ['password', 'remember_token'];
+
+    public function acceptProject(Request $request)
+    {
+        // Gather Project and Team Member info
+        $userId = session()->get('login_' . md5("Illuminate\Auth\Guard"));
+        $user = User::where('id', $userId)->first();
+        $role = $request->role;
+        $projectId = $request->project_id;
+        $boardId = $request->input('board');
+        
+        // Add to team member table & trello board
+        TeamMember::insert([
+            'user_id' => $userId,
+            'role' => $role,
+            'project_id' => $projectId
+            ]);
+        $teamMembers = TeamMember::where('project_id', $projectId)->count();
+
+        if ($teamMembers >= 2)
+        {
+            // create trello board
+        } else
+        {
+            // add user to trello board
+        }
+        Project::insert([
+            'trello_id' => $boardId,
+            ]);
+        User::where('id', $userId)->update(['queue' => null]);
+
+        return view("alumni.trello")->with('boardId', $boardId);
+    }
 }

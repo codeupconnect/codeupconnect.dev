57117280fac8ca1da58b077ebad0eabcbbb74043
Ashley Marie Callahan <ashley.callahan1091@gmail.com>
2016-09-09 15:27:37 -0500

    styling all the things

--- resources/views/alumni/user-project-invite.blade.php
+++ resources/views/alumni/user-project-invite.blade.php
@@ -85,7 +85,20 @@
 				</li>
 			</ul>
 		</div>
-		
+		<div class="container button-container col-xs-12">
+	 		<div class="col-xs-4">
+	 			<form method="POST" action="{{ action('UsersController@acceptInvite') }}">
+	 	   			{!! csrf_field() !!}  
+	 				<button name="id" value="{{ $user->id }}" class="btn btn-success" >Accept</button>
+	 			</form>
+	 		</div>
+	 		<div class="col-xs-4">
+	 			<form method="POST" action="{{ action('UsersController@rejectInvite') }}">
+	 		    	{!! csrf_field() !!}    
+	 				<button name="id" value="{{ $user->id }}" class="btn btn-danger" >Pass</button>
+	 			</form>
+	 		</div>
+ 		</div>
 	</div>
 </div>
 

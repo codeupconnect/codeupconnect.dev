9e463a22724775185fa2712fe162afced7374604
Eddie Knight <iv.eddieknight@gmail.com>
2016-09-12 09:58:42 -0500

    resolving merge conflict

--- config/services.php
+++ config/services.php
@@ -1,7 +1,5 @@
 <?php
-
 return [
-
     /*
     |--------------------------------------------------------------------------
     | Third Party Services
@@ -13,28 +11,23 @@ return [
     | to have a conventional place to find your various credentials.
     |
     */
-
     'mailgun' => [
         'domain' => env('MAILGUN_DOMAIN'),
         'secret' => env('MAILGUN_SECRET'),
     ],
-
     'mandrill' => [
         'secret' => env('MANDRILL_SECRET'),
     ],
-
     'ses' => [
         'key'    => env('SES_KEY'),
         'secret' => env('SES_SECRET'),
         'region' => 'us-east-1',
     ],
-
     'stripe' => [
         'model'  => App\User::class,
         'key'    => env('STRIPE_KEY'),
         'secret' => env('STRIPE_SECRET'),
     ],
-
     'github' => [
     'client_id' => '3c25aec002c53c5cdb31',
     'client_secret' => '6f0e41d1184bd40337622ba959a67d898943ebe2',

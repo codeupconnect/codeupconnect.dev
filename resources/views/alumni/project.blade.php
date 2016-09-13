cad948b56c4c1f9c8e388ece28a1741bfae3bd59
Eddie Knight <iv.eddieknight@gmail.com>
2016-09-13 10:04:40 -0500

    styling close project button

--- resources/views/alumni/project.blade.php
+++ resources/views/alumni/project.blade.php
@@ -7,7 +7,7 @@
 @include('partials.back-to-projects-btn')
 
 <div>
-	<a href="{{ action('UsersController@closeProject') }}" class="btn btn-danger" id="close-project">Back to Projects</a>
+	<a href="{{ action('UsersController@closeProject') }}" class="btn btn-danger" id="close-project">Close Project</a>
 </div>
 
 @section('bottom-scripts')

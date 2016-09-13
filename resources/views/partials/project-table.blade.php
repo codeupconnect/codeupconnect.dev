8fbb5eb764e36b5871931b11a7f9a17574b4ebce
Eddie Knight <iv.eddieknight@gmail.com>
2016-09-13 10:22:30 -0500

    fixing project view

--- resources/views/partials/project-table.blade.php
+++ resources/views/partials/project-table.blade.php
@@ -21,3 +21,20 @@
 		</tbody>
 	</table>		
 </div>
+<div>
+	<table class="table table-bordered table-striped table-hover">
+		<tbody>
+			<tr class="table-row"><td>Point Person</td>
+				<td>Phone</td>
+				<td>Email</td>
+				<td>Additional Details</td>
+			</tr>
+			<tr class="table-row">
+				<td>{{ $project->point_person }}</td>
+				<td>{{ $project->phone }}</td>
+				<td>{{ $project->email }}</td>
+				<td>{{ $project->project_details }}</td>
+			</tr>
+		</tbody>
+	</table>		
+</div>

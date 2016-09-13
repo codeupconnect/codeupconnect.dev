96c0e56731c4254693742f58bf21ce59378f88c5
Ashley Marie Callahan <ashley.callahan1091@gmail.com>
2016-09-13 09:42:22 -0500

    removed typo

--- resources/views/public/welcome.blade.php
+++ resources/views/public/welcome.blade.php
@@ -113,11 +113,5 @@
 	        });
 	    });
 	    
-	    $('.page-scroll a').bind('click', function(event) {
-        var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: ($($anchor.attr('href')).offset().top - 50)
-        }, 1250, 'easeInOutExpo');
-        event.preventDefault();
-    });s
+	    
 	</script>

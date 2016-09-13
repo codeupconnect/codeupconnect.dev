39a93b9859ef36b883bc004adc30ed7b39151dc8
Eddie Knight <iv.eddieknight@gmail.com>
2016-09-13 10:29:06 -0500

    fixing project view

--- resources/views/partials/project-table.blade.php
+++ resources/views/partials/project-table.blade.php
@@ -4,19 +4,11 @@
 			<td>Site URL</td>
 			<td>Desired Start</td>
 			<td>Launch Date</td>
-			<td>Point Person</td>
-			<td>Phone</td>
-			<td>Email</td>
-			<td>Additional Details</td>
 		<tbody>
 			<tr class="table-row">
 				<td>{{ $project->site_url }}</td>
 				<td>{{ $project->start_date }}</td>
 				<td>{{ $project->end_date }}</td>
-				<td>{{ $project->point_person }}</td>
-				<td>{{ $project->phone }}</td>
-				<td>{{ $project->email }}</td>
-				<td>{{ $project->project_details }}</td>
 			</tr>
 		</tbody>
 	</table>		
@@ -25,19 +17,19 @@
 	<table class="table table-bordered table-striped table-hover">
 		<tbody>
 			<tr class="table-row">
-				<td>Point Person</td>
+				<td><h3>Point Person</h3></td>
 				<td>{{ $project->point_person }}</td>
 		
 			<tr class="table-row">	
-				<td>Phone</td>
+				<td><h3>Phone</h3></td>
 				<td>{{ $project->phone }}</td>
 			
 			<tr class="table-row">	
-				<td>Email</td>
+				<td><h3>Email</h3></td>
 				<td>{{ $project->email }}</td>
 			
 			<tr class="table-row">	
-				<td>Additional Details</td>
+				<td><h3>Additional Details</h3></td>
 				<td>{{ $project->project_details }}</td>
 			</tr>
 		</tbody>

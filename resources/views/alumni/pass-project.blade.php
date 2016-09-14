b8344af07853f80b4207552041671fa6fb57ecda
Eddie Knight <iv.eddieknight@gmail.com>
2016-09-14 11:44:30 -0500

    added view for cannotPass()

---
+++ resources/views/alumni/pass-project.blade.php
@@ -0,0 +1,12 @@
+@extends('layout.master')
+
+@section('content')
+
+<div class="container admin text-center">
+	<h2 class='font-thin slate-font' id="header-spacing">Oops!</h2>
+	<div class="edit">
+		<h3 class='font-thin slate-font'>No one is available in the queue to pass this project to!</h3>
+		<h4 class='font-thin slate-font'>Please accept this project or try again later.</h4>
+	</div>
+</div>
+@stop
